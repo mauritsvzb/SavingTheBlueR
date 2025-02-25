526f411ccdda3659b6bb353e3ceaa9a632b11b34
Maurits van Zinnicq <mauritsvzb@gmail.com>
2025-02-25 08:48:35 +0100

    added some more functionality e.g. more functions

--- R/02_data-compilation.R
+++ R/02_data-compilation.R
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------
 # Script: Compile and Filter Acoustic Detection Data
-# Author: Sprinkles
+# Author: Maurits van Zinnicq Bergmann
 # Date:   2025-02-24
 #
 # Description:
@@ -49,10 +49,81 @@ import_and_preprocess_data <- function(datadir, timezone) {
   VMOV <- readRDS(file.path(datadir, "VMOV.rds"))
 
   # Import individual data
-  IND <- readRDS(file.path(datadir, "/IND.rds"))
+  IND <- readRDS(file.path(datadir, "IND.rds"))
 
   list(det = det, VMOV = VMOV, IND = IND)
 }
+
+#-------------------------------------------------------------------------------
+# Function: filter_detections_by_tag_deployment
+#-------------------------------------------------------------------------------
+#' @description Filter to remove detections that may have been biased by tagging event
+#' @param det Detection dataframe
+#' @param IND Individual dataframe
+#' @param filter Boolean to indicate if first 24 hours following tag deployoment should be removed
+#' @return Filtered detection dataframe
+filter_detections_by_tag_deployment <- function(det, IND, filter = FALSE) {
+  if (filter) {
+    IND <- IND %>% mutate(tagging_datetime = tagging_datetime + hours(24))
+  }
+
+  det %>%
+    semi_join(IND, by = c("elasmo" = "acoustic_tag_id")) %>%
+    filter(time >= IND$tagging_datetime[match(elasmo, IND$acoustic_tag_id)])
+}
+
+#-------------------------------------------------------------------------------
+# Function: assign_locations_to_detections
+#-------------------------------------------------------------------------------
+#' @description Assigns locations to detections based on receiver deployment periods
+#' @param det Detection dataframe
+#' @param VMOV Receiver movement dataframe
+#' @return Detection dataframe with assigned locations
+assign_locations_to_detections <- function(det, VMOV) {
+  det %>%
+    left_join(
+      VMOV %>%
+        select(`Receiver ID`, STATION_NO, `Date In`, `Date Out`) %>%
+        rename(station = `Receiver ID`, location = STATION_NO),
+      by = "station"
+    ) %>%
+    filter(time >= `Date In`, time <= `Date Out`) %>%
+    select(-`Date In`, -`Date Out`)
+}
+
+#-------------------------------------------------------------------------------
+# Function: compile_data
+#-------------------------------------------------------------------------------
+#' @description Main function to compile and filter acoustic detection data
+#' @param datadir Directory containing the data files
+#' @param timezone Timezone for the data
+#' @param filter Boolean to indicate if first 24 hours should be filtered out
+#' @return Compiled and filtered detection dataframe
+compile_data <- function(datadir, timezone, filter = FALSE) {
+  # Import and preprocess data
+  data <- import_and_preprocess_data(datadir, timezone)
+
+  # Filter detections by tag deployment
+  filtered_df <- filter_detections_by_tag_deployment(data$df, data$IND, filter)
+
+  # Assign locations to detections
+  compiled_df <- assign_locations_to_detections(filtered_df, data$VMOV)
+
+  # Remove detections without assigned locations
+  compiled_df <- compiled_df %>% filter(!is.na(location))
+
+  # Save compiled data
+  saveRDS(compiled_df, file.path(datadir, "DET_compiled.rds"))
+
+  compiled_df
+}
+
+
+
+
+
+
+
 
 
 #-------------------------------------------------------------------------------
@@ -60,7 +131,7 @@ import_and_preprocess_data <- function(datadir, timezone) {
 #-------------------------------------------------------------------------------
 
 # Run the compile function
-compiled_data <- compile_data(dir, filter = FALSE)
+compiled_data <- compile_data(datadir, timezone, filter = FALSE)
 
 # Print summary of compiled data
 cat("Compilation complete. Summary of compiled data:\n")
